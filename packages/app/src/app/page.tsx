99fe1093191c0b13e2e94578c47a5519a2aaacd8
Ayush Gupta <7221434+ayushgupta0610@users.noreply.github.com>
2024-10-19 18:40:29 +0530

    Make fetch env agnostic

--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -10,7 +10,8 @@ export default async function Home() {
   if (!process.env.NEXT_PUBLIC_TALENT_API_KEY) {
     throw new Error('TALENT_API_KEY is not defined in the environment variables.')
   }
-  const res = await fetch('http://localhost:3000/api/talent?page=1', { headers }) // Adjust the API endpoint as needed
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000' // Default to localhost if not set
+  const res = await fetch(`${apiUrl}/api/talent?page=1`, { headers }) // Use the relative API URL
   const data: PassportResponse = await res.json()
   return <AirdropPage initialData={data} />
 }
